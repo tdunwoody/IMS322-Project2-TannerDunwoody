5e2991c66648e8c39de01e7f47bca27fc9ce98bd
tdunwoody <89400718+tdunwoody@users.noreply.github.com>
2021-10-27 10:17:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project 2 (change to your project name)
+
+## Description
+Enter project description here.
---
+++ main.js
@@ -0,0 +1,11 @@
+// Project 2 JavaScript
+
+const VueApp = {
+    data() {
+      return {
+        message: "Vue is working!"
+      }
+    }
+  }
+  
+Vue.createApp(VueApp).mount('#hello-vue')
