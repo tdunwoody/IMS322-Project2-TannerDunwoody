fd784dda536c1dd03c42e073d2f0afe854cc7ec0
IsaacBonham <bonhamib@miamioh.edu>
2021-11-22 11:24:37 -0500

    replacement functions

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let guideImage = document.getElementById("guideImage");
+let guideText = document.getElementById("guideText");
 
 function stepOne(){
-  
+  guideImage.src ="epic play 2.PNG"
+  guideText.innerHTML = "da text"
 }
-
 function stepTwo(){
   
 }
