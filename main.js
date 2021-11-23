02d96381e6f45eaa0901558453c4bd0b5ad5bcf4
IsaacBonham <bonhamib@miamioh.edu>
2021-11-23 17:16:36 -0500

    functions to change text and picture

--- main.js
+++ main.js
@@ -3,26 +3,31 @@ let guideText = document.getElementById("guideText");
 
 function stepOne(){
   guideImage.src ="epic play 2.PNG"
-  guideText.innerHTML = "da text"
+  guideText.innerHTML = "step 1 text"
 }
 function stepTwo(){
-  
+  guideImage.src ="epic play 2.PNG"
+  guideText.innerHTML = "step 2 text"
 }
 
 function stepThree(){
-  
+  guideImage.src ="epic play 2.PNG"
+  guideText.innerHTML = "step 3 text"
 }
 
 function stepFour(){
-  
+  guideImage.src ="epic play 2.PNG"
+  guideText.innerHTML = "step 4 text"
 }
 
 function stepFive(){
-  
+  guideImage.src ="epic play 2.PNG"
+  guideText.innerHTML = "step 5 text"
 }
 
 function stepSix(){
-  
+  guideImage.src ="epic play 2.PNG"
+  guideText.innerHTML = "step 6 text"
 }
 
 function stepQuiz(){
